263e1d1f409b846672a092c55915652eea324938
Juan Camilo Zarta Campo <juancamilozartacampo@gmail.com>
2025-05-06 16:39:26 -0500

    Acomodado cerrar seccion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -196,7 +196,7 @@ export class AuthService {
         // window.location.href = '/miCartera/login'; // Redirigir al login
         window.location.href = `${document.baseURI}login`;
 
-      }, 2 * 60 * 1000); // 2 minutos
+      }, 5 * 60 * 1000); // 2 minutos
     };
 
     window.addEventListener('mousemove', resetTimer);
